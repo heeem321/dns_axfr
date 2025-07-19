d580b0c9e64a92bafe21e0def78e152444ccef6b
Ebrahim <heeem321@hotmail.com>
2025-07-19 20:11:33 +0800

    DNS enumeration v1.0 -- Initial commit

---
+++ dns_axfr.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+# Dependencies:
+# python3-dnspython
+
+
+# Modules Imports:
+import dns.zone as dz
+import dns.query as dq
+import dns.resolver as dr
+import argparse
+
+
+# Initialization:
+NS = dr.Resolver()
+
+# List of found subdomains:
+Subdomains = []
+
+def AXFR(domain, nameserver):
+    # Try zone transfer for a given domain and nameserver
+    try:
+        # Perform the zone transfer
+        axfr = dz.from_xfr(dq.xfr(nameserver, domain))
+
+        # If zone transfer was successful
+        if axfr:
+            print('[*] Sucessful Zone Transfer from {}'.format(nameserver))
+            # Add found subdomains  to global 'Subdomain' list
+            for record in axfr:
+                Subdomains.append('{}.{}'.format(record.to_text(), domain))
+
+        # If zone transfer fails
+    except Exception as error:
+        print(error)
+        pass
+
+# Main
+if __name__=="__main__":
+
+
+    # ArgParser - Define usage
+    parser = argparse.ArgumentParser(prog="dns-axfr.py", epilog="DNS Zonetransfer Script", usage="dns-axfr.py [options] -d <DOMAIN>", prefix_chars='-', add_help=True)
+
+    # Positional Arguments
+    parser.add_argument('-d', action='store', metavar='Domain', type=str, help='Target Domain.\tExample: inlanefreight.htb', required=True)
+    parser.add_argument('-n', action='store', metavar='Nameserver', type=str, help='Nameservers separated by a comma.\tExample: ns1.inlanefreight.htb,ns2.inlanefreight.htb')
+
+    parser.add_argument('-v', action='version', version='DNS-AXFR - v1.0', help='Prints the version of DNS-AXFR.py')
+
+    # Assign given arguments
+    args = parser.parse_args()    
+
+    # Check if URL is given
+    if not args.d:
+        print('[!] You must specify target Domain.\n')
+        print(parser.print_help())
+        exit()
+
+    if not args.n:
+        print('[!] You must specify target nameservers.\n')
+        print(parser.print_help())
+        exit()
+
+        # Variables
+    Domain = args.d
+    NS.nameservers = list(args.n.split(","))
+
+    # For each nameserver
+    for nameserver in NS.nameservers:
+
+        # Try AXFR
+        AXFR(Domain, nameserver)
+
+    # Print the results
+    if Subdomains is not None:
+        print('------- Found Subdomains:')
+
+
+        # Print each subdomain
+        for subdomain in Subdomains:
+            print('{}'.format(subdomain))
+
+        else:
+            print('No subdomains found.')
+            exit()
